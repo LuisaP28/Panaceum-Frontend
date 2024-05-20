91aa6164a4d9b1d1465bb4935faed8b288d61f8c
Keity <keityortegam@gmail.com>
2024-05-20 16:09:05 -0500

    Modificación de la vista Datos

--- Js/Datos/datos.js
+++ Js/Datos/datos.js
@@ -1,14 +1,14 @@
 //Botón select//
-document.addEventListener('DOMContentLoaded', function() {
-    const dropdownMenu = document.querySelector('.dropdown-menu');
-    const selectedOption = document.getElementById('selectedOption');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const dropdownMenu = document.querySelector('.dropdown-menu');
+//     const selectedOption = document.getElementById('selectedOption');
 
-    dropdownMenu.addEventListener('click', function(event) {
-        const target = event.target;
+//     dropdownMenu.addEventListener('click', function(event) {
+//         const target = event.target;
 
-        if (target.classList.contains('dropdown-item')) {
-            const selectedValue = target.textContent;
-            selectedOption.value = selectedValue;
-        }
-    });
-});
+//         if (target.classList.contains('dropdown-item')) {
+//             const selectedValue = target.textContent;
+//             selectedOption.value = selectedValue;
+//         }
+//     });
+// });
